b13b2c6f8cd3af5e614ec83be030fb530c441c80
coderCF <1559878532@qq.com>
2021-03-22 13:03:02 +0800

    创建项目
    初始化项目

---
+++ HorizontalCategory/HorizontalCategory/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HorizontalCategory
+//
+//  Created by Mac on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
